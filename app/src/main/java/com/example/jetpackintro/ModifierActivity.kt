e301b0bb18928e116193aafa825f1d862c538138
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-07-24 23:55:49 +0530

    commit state

--- app/src/main/java/com/example/jetpackintro/ModifierActivity.kt
+++ app/src/main/java/com/example/jetpackintro/ModifierActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,6 +23,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,33 +33,39 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackintro.ui.theme.Jade
 import com.example.jetpackintro.ui.theme.Pink
+import kotlin.random.Random
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Pink)
-                    .fillMaxHeight(0.8f)
-                    .fillMaxWidth()
-//                    .requiredWidth(600.dp)
-                    .padding(10.dp)
-                    .border(3.dp , Color.Cyan, shape = RoundedCornerShape(15.dp))
-                    .padding(top = 32.dp, start = 64.dp)
-            ) {
-                Text(text = "JetPack")
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(text = "Compose")
-                Text("Modifiers" ,
+            Column (modifier = Modifier.fillMaxSize()){
+                Column(
                     modifier = Modifier
-                        .offset(y = 20.dp) //won't push other elements  like margin
-                        .clickable {  }
-                        .background(Jade)
-                        .border(2.dp ,Color.Cyan)
+                        .background(Pink)
+                        .fillMaxHeight(0.8f)
+                        .fillMaxWidth()
+//                    .requiredWidth(600.dp)
                         .padding(10.dp)
-                        )
+                        .border(3.dp, Color.Cyan, shape = RoundedCornerShape(15.dp))
+                        .padding(top = 32.dp, start = 64.dp)
+                ) {
+                    Text(text = "JetPack")
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Text(text = "Compose")
+                    Text("Modifiers" ,
+                        modifier = Modifier
+                            .offset(y = 20.dp) //won't push other elements  like margin
+                            .clickable { }
+                            .background(Jade)
+                            .border(2.dp, Color.Cyan)
+                            .padding(10.dp)
+                    )
+                }
+                Colorbox(
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
@@ -63,3 +73,22 @@ class MainActivity2 : ComponentActivity() {
     private fun enableEdgeToEdge() {}
 }
 
+@Composable
+fun Colorbox(modifier: Modifier = Modifier){      // recomposed to show state change
+    val color = remember { mutableStateOf(Color.Cyan) }  // won't reset after recomposition
+
+    Box (
+        modifier = modifier
+            .background(color.value)
+            .clickable {
+                color.value = Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f   // opacity
+                )
+            }
+    ){
+
+    }
+}
