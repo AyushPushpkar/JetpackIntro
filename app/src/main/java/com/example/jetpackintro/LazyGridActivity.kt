8ec9a0c7d82a5f5e9898ede2ab6d65f96b564f0a
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-08-07 19:48:06 +0530

    Lazy grid

---
+++ app/src/main/java/com/example/jetpackintro/LazyGridActivity.kt
@@ -0,0 +1,59 @@
+package com.example.jetpackintro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackintro.ui.theme.JetPackIntroTheme
+
+class LazyGridActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetPackIntroTheme {
+                val state = rememberLazyGridState(
+                    initialFirstVisibleItemIndex = 69
+                )
+
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(100.dp) ,
+                    state = state,
+                    content = {
+                        items(100){
+                            Box(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .aspectRatio(1f)
+                                    .clip(RoundedCornerShape(5.dp))
+                                    .background(Color.Green) ,
+                                contentAlignment = Alignment.Center
+                            ){
+                                Text(text = "item $it")
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    }
+    private fun  enableEdgeToEdge(){}
+}
