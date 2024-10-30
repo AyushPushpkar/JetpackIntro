6c6bfaf34d796b389aeff388ceae405ac55fe094
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-10-30 15:54:15 +0530

    just commit

--- app/src/main/java/com/example/jetpackintro/RowColumnActivity.kt
+++ app/src/main/java/com/example/jetpackintro/RowColumnActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
